82ea7ef9bc874c287c601297cdf13fa9e5b6e6af
Kunštek Antonio <kunstekantonio68@gmail.com>
2023-07-24 13:14:16 +0200

    Added constant color palettes

---
+++ lib/commons/colors/text_colors.dart
@@ -0,0 +1,15 @@
+library diplomski_rad_assets_module;
+
+import 'package:flutter/material.dart';
+
+class TextColors {
+  static const Color mainTextColor = Color(0xfff7f7f7);
+  static const Color secondaryTextColor = Color(0xFF0c1b33);
+
+  static const Color whiteTextColor = Color(0xFFeeeeee);
+
+  static const Color successTextColor = Color(0xff4a7c59);
+  static const Color errorTextColor = Color(0xffBB4430);
+  static const Color infoTextColor = Color(0xff7EBDC2);
+  static const Color warningTextColor = Color(0xffDB9D47);
+}
