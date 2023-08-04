3bfd16d43e1498fefee228e88a055b3b06eaaaf4
Kunštek Antonio <kunstekantonio68@gmail.com>
2023-08-04 22:31:14 +0200

    t3

--- lib/network/rest.dart
+++ lib/network/rest.dart
@@ -4,26 +4,6 @@ import 'package:diplomski_rad_assets_module/models/account_model.dart';
 import 'package:diplomski_rad_assets_module/network/response.dart';
 import 'package:http/http.dart' as http;
 
-Future<List<Account>> fetchAllAccounts(String? jwtToken) async {
-  http.Response res = await http.get(Uri.parse('http://10.0.2.2:9999/api/v1/account/all'),
-      headers: <String, String> {
-        'Content-type': 'application/json; charset=UTF-8',
-        'Authorization': 'Bearer $jwtToken'
-      },
-  );
-
-  if(res.statusCode == 200) {
-    Response res2 = Response.fromJson(jsonDecode(res.body));
-    if(res2.successful) {
-      return getAccountList(res2.data);
-    } else {
-      throw Exception("");
-    }
-  } else {
-    throw Exception("");
-  }
-}
-
 List<Account> getAccountList(List<dynamic> responseData) {
   List<Account> accountList = [];
 
@@ -34,4 +14,23 @@ List<Account> getAccountList(List<dynamic> responseData) {
   }
 
   return accountList;
+}
+
+Future<List<Account>> fetchAllAccounts(String? jwtToken) async {
+  http.Response res = await http.get(
+    Uri.parse('http://10.0.2.2:9999/api/v1/account/all'),
+    headers: <String, String>{
+      'Content-type': 'application/json; charset=UTF-8',
+      'Authorization': 'Bearer $jwtToken'
+    },
+  );
+
+  if (res.statusCode == 200) {
+    Response res2 = Response.fromJson(jsonDecode(res.body));
+    if (res2.successful) {
+      return getAccountList(res2.data);
+    }
+    return [];
+  }
+  return [];
 }
